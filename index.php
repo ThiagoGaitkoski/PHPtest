d28446ecdc27398ee11b196010740c63ec3f9621
Thiago Gaitkoski <68345301+ThiagoGaitkoski@users.noreply.github.com>
2021-10-04 13:54:13 -0300

    Add bootstrap

--- index.php
+++ index.php
@@ -8,19 +8,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Pesquisa CEP</title>
 </head>
     <body> 
-        <form action="." method="post">
-            <p>Digite o CEP a ser procurado</p>
-            <input type="text" name="cep" placeholder="Digite o CEP..." value="<?php echo $address->cep ?>" required>
-            <button type="submit">Procurar</button>
+    <div class="container mt-5">
+        <h1>Pesquisa CEP</h1>
+        <form class="row g-3" action="." method="post">
+            <div class="col-md-6">
+                <input type="text" class="form-control" placeholder="Digite o CEP..." name="cep" value="<?php echo $address->cep ?>" required>
+            </div>
+            <div class="col-md-12">
+                <button type="submit" class="btn btn-primary">Pesquisar</button>
+            </div>
+            
+            <div class="col-md-6">
+                <label for="Rua" class="form-label">Rua</label>
+                <input type="text" class="form-control" id="Rua" value="<?php echo $address->logradouro ?>" disabled>
+            </div>
+            <div class="col-md-8">
+                <label for="Complemento" class="form-label">Complemento</label>
+                <input type="texte" class="form-control" id="Complemento" name="complemento" value="<?php echo $address->complemento ?>" disabled>
+            </div>
+            <div class="col-md-4">
+                <label for="Bairro" class="form-label">Bairro</label>
+                <input type="text" class="form-control" id="Bairro" value="<?php echo $address->bairro ?>" disabled>
+            </div>
 
-            <input type="text" name="rua" placeholder="Rua" value="<?php echo $address->logradouro ?>" disabled>
-            <input type="text" name="complemento" placeholder="Complemento" value="<?php echo $address->complemento ?>" disabled>
-            <input type="text" name="bairro" placeholder="Bairro" value="<?php echo $address->bairro ?>" disabled>
-            <input type="text" name="cidade" placeholder="Cidade" value="<?php echo $address->localidade ?>" disabled>
-            <input type="text" name="estado" placeholder="Estado" value="<?php echo $address->uf ?>" disabled>
+            <div class="col-md-10">
+                <label for="Cidade" class="form-label">Cidade</label>
+                <input type="texte" class="form-control" id="Cidade" name="cidade" value="<?php echo $address->localidade ?>" disabled>
+            </div>
+            <div class="col-md-2">
+                <label for="Estado" class="form-label">Estado</label>
+                <input type="text" class="form-control" id="Estado" name="estado" value="<?php echo $address->uf ?>" disabled>
+            </div>
         </form>
+    </div>
     </body>
 </html>
+
+
